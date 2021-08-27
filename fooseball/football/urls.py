dd76c88ef289017a32ae6f82b5d08cc460d0939a
Roberto C <rcapurro@unomaha.edu>
2021-08-27 13:56:54 -0500

    updates

--- fooseball/football/urls.py
+++ fooseball/football/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ScoreListView, ScoreCreateView
 
 urlpatterns = [
-    path('scores/', ScoreListView.as_view()),
-    path('scores/new_score', ScoreCreateView.as_view(), name='new_score'),
+    path('', ScoreListView.as_view(), name=''),
+    path('new_score', ScoreCreateView.as_view(), name='new_score'),
 ]
